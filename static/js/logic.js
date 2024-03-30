b0053e9d719ea8c04193b0b7f89e442c45aea98c
downleft <downleft@jodys-air.attlocal.net>
2024-03-30 18:59:48 -0400

    Folder Creation

---
+++ static/js/logic.js
@@ -0,0 +1,3 @@
+// Part 1: Create an Earthquake Visualization
+// Get the All Earthquakes in Last Month link: 
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
