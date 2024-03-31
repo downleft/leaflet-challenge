1f8ed20bd149f53bb9c7da10ba94dd45bfea16eb
downleft <downleft@jodys-air.attlocal.net>
2024-03-31 19:44:36 -0400

    Markers created

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,58 @@
 // Part 1: Create an Earthquake Visualization
-// Get the All Earthquakes in Last Month link: 
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+// Get the All Earthquakes in Last Week or Last Month link: 
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 // Perform GET request
-// d3.json(url).then(function (data) {
-//     // Await response, then send the data.features object to the createFeatures function.
-//     createFeatures(data.features);
-//   });
+d3.json(url).then(function (data) {
+    // Await response, then send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+  });
   
-function createMap() {
+  function createFeatures(earthquakeData) {
+
+    // Define arrays to hold the created earthquake markers.
+    let earthquakeMarkers = [];
+    console.log(earthquakeData[0].properties.mag)
+
+    // Loop through locations, and create the city and state markers.
+    for (let i = 0; i < earthquakeData.length; i++) {
+        
+        // Establish coordinate for earthquake
+        let latitude = earthquakeData[i].geometry.coordinates[1]
+        let longitude = earthquakeData[i].geometry.coordinates[0]
+        let coordinates = [latitude, longitude]
+        // Setting the marker radius for the state by magnitude
+        earthquakeMarkers.push(
+            L.circle(coordinates, {
+                stroke: false,
+                fillOpacity: 0.75,
+                color: "green",
+                fillColor: "green",
+                radius: earthquakeData[i].properties.mag * 50000
+            })
+            );
+        }
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+  
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakeMarkers);
+  }
+
+function createMap(earthquakeMarkers) {
+    
+
     // Create the base layers.
   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -18,10 +62,18 @@ function createMap() {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
+  // Create layer group for earthquake markers
+  let quakeLayer = L.layerGroup(earthquakeMarkers)
+
   // Create a baseMaps object.
   let baseMaps = {
     "Street Map": street,
     "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: quakeLayer
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
@@ -30,7 +82,7 @@ function createMap() {
       37.09, -95.71
     ],
     zoom: 4,
-    layers: [street]
+    layers: [street, quakeLayer]
   });
   
 
@@ -38,7 +90,7 @@ function createMap() {
 // Pass it our baseMaps and overlayMaps.
 // Add the layer control to the map.
   L.control.layers(baseMaps, 
-    //overlayMaps, 
+    overlayMaps, 
     {collapsed: false}).addTo(myMap);
 };
-createMap()
+
